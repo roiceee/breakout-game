8a42582fb042170c40c11f5d735b973e73971ae4
John Roice Aldeza <jroicealdeza@gmail.com>
2024-11-25 23:07:14 +0800

    Refactor RoundPage component for improved layout and styling

--- src/components/RoundPage.tsx
+++ src/components/RoundPage.tsx
@@ -5,18 +5,13 @@ import InstructionCard from "./InstructionCard";
 export default function RoundPage({ data }: { data: RoundType }) {
   return (
     <div className="flex flex-col items-center gap-2">
-      <div className="flex flex-col xl:flex-row items-start justify-center gap-2 w-full">
-        <div className="card border-2 shadow-lg w-full mx-auto xl:mx-0 bg-base-100">
-          <div className="card-body p-2 flex-row justify-center">
+        <div className="card border-2 p-2 shadow-lg w-full mx-auto xl:mx-0 bg-base-100">
+          <div className="card-body p-2 justify-center flex-col xl:flex-row gap-6">
             <img src={data.imageUrl} className="max-h-[60vh] mx-auto w-auto " />
+            <InstructionCard data={data} className="w-full" />
           </div>
 
-          <AnswerCard
-            className="mt-2"
-            data={data}
-          />
-        </div>
-        <InstructionCard data={data} className="w-full" />
+          <AnswerCard className="mt-2" data={data} />
       </div>
     </div>
   );
