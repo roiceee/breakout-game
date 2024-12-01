658ea4bdf02d903d3536d5f794f8feccf91fabbd
John Roice Aldeza <jroicealdeza@gmail.com>
2024-12-01 15:39:47 +0800

    Enable HTML rendering in InstructionCard for dynamic instruction content

--- src/components/InstructionCard.tsx
+++ src/components/InstructionCard.tsx
@@ -11,14 +11,20 @@ interface Props {
 
 export default function InstructionCard({ data, className }: Props) {
   return (
-    <div
-      className={`card p-1 ${className}`}
-    >
+    <div className={`card p-1 ${className}`}>
       <div className="card-body p-3">
         <RoundDisplayCard className="m-2 w-fit" />
         <h2 className="card-title my-2">{data.title}</h2>
 
-        <p className="text-xl">{data.instruction}</p>
+        <p className="text-xl">
+          {/* accept html tags */}
+
+          <div
+            dangerouslySetInnerHTML={{
+              __html: data.instruction
+            }}
+          ></div>
+        </p>
       </div>
     </div>
   );
